98a50da06d31b7a0e5b715bf912b4aae42101c86
Sam Turrell <sam.turrell@netsells.co.uk>
2018-01-31 22:32:08 +0000

    jsdoc Adds jsdoc rules

--- rules/javascript.js
+++ rules/javascript.js
@@ -4,5 +4,17 @@ module.exports = {
     rules: {
         // Requires trailing commas when the last element or property is in a different line than the closing ] or } and disallows trailing commas when the last element or property is on the same line as the closing ] or }
         'comma-dangle': [_THROW.ERROR, 'only-multiline'],
+        // Require JSDoc on all functions and classes
+        'require-jsdoc': [_THROW.WARNING, {
+            "require": {
+                "FunctionDeclaration": true,
+                "MethodDefinition": true,
+                "ClassDeclaration": true,
+                "ArrowFunctionExpression": true,
+                "FunctionExpression": true,
+            },
+        }],
+        // Require jsdoc data to be consistently valid
+        'valid-jsdoc': [_THROW.WARNING],
     },
 }
