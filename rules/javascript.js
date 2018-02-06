eab46f5b9e72b39c5a4717cfc9b964eab489cefe
Neveena Ferrao <neveena.ferrao@netsells.co.uk>
2018-02-06 11:16:52 +0000

    added space at the start of the comment

--- rules/javascript.js
+++ rules/javascript.js
@@ -2,7 +2,7 @@ const _THROW = require('../modules/throwables');
 
 module.exports = {
     rules: {
-        //Disallow mixed spaces and tabs for indentation
+        // Disallow mixed spaces and tabs for indentation
         'no-mixed-spaces-and-tabs': [_THROW.WARNING],
         // Requires trailing commas when the last element or property is in a different line than the closing ] or } and disallows trailing commas when the last element or property is on the same line as the closing ] or }
         'comma-dangle': [_THROW.ERROR, 'only-multiline'],
