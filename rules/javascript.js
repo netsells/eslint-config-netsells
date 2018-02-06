ccfb193b7937cbf7db75c5dd433596d251db4122
Jacob Jenkins <jacob.jenkins@netsells.co.uk>
2018-02-06 14:08:52 +0000

    rule/amend-require-jsdoc-ignore Uses jsdoc-except and adds the VueJS lifecycle methods to ignore

--- rules/javascript.js
+++ rules/javascript.js
@@ -5,7 +5,7 @@ module.exports = {
         // Requires trailing commas when the last element or property is in a different line than the closing ] or } and disallows trailing commas when the last element or property is on the same line as the closing ] or }
         'comma-dangle': [_THROW.ERROR, 'only-multiline'],
         // Require JSDoc on all functions and classes
-        'require-jsdoc': [_THROW.WARNING, {
+        'require-jsdoc-except/require-jsdoc': [_THROW.WARNING, {
             require: {
                 FunctionDeclaration: true,
                 MethodDefinition: true,
@@ -13,6 +13,17 @@ module.exports = {
                 ArrowFunctionExpression: true,
                 FunctionExpression: true,
             },
+            // Ignore all the standard VueJS lifecycle methods
+            ignore: [
+                "beforeCreate",
+                "created",
+                "beforeMount",
+                "mounted",
+                "beforeDestroy",
+                "destroyed",
+                "beforeUpdate",
+                "updated",
+            ],
         }],
         // Require jsdoc data to be consistently valid
         'valid-jsdoc': [_THROW.WARNING, {
