f4561ed5b4bb3db10b7459e7818ef1821e2c5421
Becca Anderton <rebecca.anderton@netsells.co.uk>
2018-02-09 09:09:02 +0000

    Fixing

--- rules/javascript.js
+++ rules/javascript.js
@@ -25,8 +25,9 @@ module.exports = {
         }],
         // Discourage using 'var' for creating variables - require using let/const instead
         'no-var': _THROW.ERROR,
+        // Disallow else blocks after return statements in if statements
         'no-else-return': [_THROW.WARNING, {
             allowElseIf: false,
-        }]
+        }],
     },
 }
