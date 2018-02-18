a492e55f3598db4ecd17fa336ebfeecd582e8014
Wiktor Zawierucha <wiktor.zawierucha@netsells.co.uk>
2018-02-18 20:29:26 +0000

    Converted the utils to an object of methods instead of a class

--- modules/utils.js
+++ modules/utils.js
@@ -1,18 +1,7 @@
-class Utils {
-    /**
-     * constructor
-     */
-    constructor() {
-        this.NODE_ENV = process.env.NODE_ENV;
-    }
-
-    /**
-     * isProduction - returns true if NODE_ENV is set to production
-     * @returns {boolean}
-     */
-    isProduction() {
-        return this.NODE_ENV === 'production';
-    }
-}
-
-module.exports = new Utils();
+/**
+ * isProduction - returns true if node env is set to production
+ * @returns {boolean}
+ */
+module.exports.isProduction = () => {
+    return process.env.NODE_ENV === 'production';
+};
